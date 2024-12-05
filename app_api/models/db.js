52ba3ad660d4a3da6064b1058bcd78d9ab0bf22c
RealCrabLord <gnyssl@hotmail.com>
2024-12-05 14:53:04 +0300

    Remove sensitive information and other updates

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,17 +1,4 @@
-var mongoose = require('mongoose');
-var dbURI = "mongodb+srv://South:guneyantalya07@south.plj7c.mongodb.net/"
+require('dotenv').config();  // dotenv paketini başlat
 
-mongoose.connect(dbURI)
-mongoose.connection.on("connected", function(){
-    console.log("Mongoose connected to " + dbURI)
-})
+var dbURI = process.env.DB_URI;  // .env dosyasındaki DB_URI'yi al
 
-mongoose.connection.on("disconnected", function(err){
-    console.log("kapandı")
-})
-
-process.on("SIGINT", function(){
-    mongoose.connection.close()
-    console.log("MongoDB bağlantısı kesildi, uygulama durduruldu.")
-    process.exit(0)
-})
